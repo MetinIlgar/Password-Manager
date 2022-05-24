43b57fa782106249941476f2676787f6849d8377
Metin Ilgar Mutlu <ilgarmutlu@outlook.com.tr>
2022-05-24 12:19:44 +0300

    Update
    The class name has changed to SetAccount and a new class named GetAccount has been added. With the SetAccount class, we can create a password and save it to the database in an encrypted form. With the GetAccount class, we can retrieve the encrypted password and account information from the database.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sqlite3 as sl
 import random, string
 
-class PasswordManager:
-    emailList = set()
+import base64
+import os,sys
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+class SetAccount:
     LOCASE_CHARACTERS = list(string.ascii_lowercase)
     UPCASE_CHARACTERS = list(string.ascii_uppercase)
     DIGITS = list(string.digits)
@@ -17,50 +21,107 @@ class PasswordManager:
     all_characters = LOCASE_CHARACTERS + UPCASE_CHARACTERS + DIGITS + SYMBOLS
     random.shuffle(all_characters)
 
-    @classmethod
-    def yourEmails(cls):
-        print(cls.emailList)
 
-    def __init__(self, email, passwordLenght = 12):
+    def __init__(self, email, encryption_password, passwordLenght = 12):
         self.email = email
+        self.encryption_password = encryption_password.encode()
         self.passwordLenght = passwordLenght
         self.password = ""
-        PasswordManager.emailList.add(self.email)
+        self.salt = os.urandom(32)
+        self.token = b""
+        self.link = "example.com"
+
+        self.createPassword()
+        self.encrypt()
+        self.setData()
 
     def createPassword(self):
         passList = []
-        rand_digit = random.choice(PasswordManager.DIGITS)
-        rand_upper = random.choice(PasswordManager.UPCASE_CHARACTERS)
-        rand_lower = random.choice(PasswordManager.LOCASE_CHARACTERS)
-        rand_symbol = random.choice(PasswordManager.SYMBOLS)
+        rand_digit = random.choice(SetAccount.DIGITS)
+        rand_upper = random.choice(SetAccount.UPCASE_CHARACTERS)
+        rand_lower = random.choice(SetAccount.LOCASE_CHARACTERS)
+        rand_symbol = random.choice(SetAccount.SYMBOLS)
 
         passList.extend([rand_digit, rand_upper, rand_lower, rand_symbol])
 
         for i in range(self.passwordLenght - 4):
-            passList.append(random.choice(PasswordManager.all_characters))
+            passList.append(random.choice(SetAccount.all_characters))
             random.shuffle(passList)
 
         for x in passList:
             self.password += x
 
 
+    def encrypt(self):
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=self.salt,
+            iterations=390000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(self.encryption_password))
+        f = Fernet(key)
+        self.token = f.encrypt(self.password.encode())
+
+    def setData(self):
+        with sl.connect('PasswordDatabase.sqlite') as db:
+            cur = db.cursor()
+            cur.execute("CREATE TABLE IF NOT EXISTS Password_Book (Link, Email, Password, Salt)")
+            cur.execute(
+                "INSERT INTO Password_Book VALUES (? ,? ,? ,? )", (self.link, self.email, self.token, self.salt))
+            db.commit()
 
 
+class GetAccount:
+    def __init__(self, encryption_password, website):
+        self.encryption_password = encryption_password.encode()
+        self.password = ""
+        self.website = website
+
+        with sl.connect('PasswordDatabase.sqlite') as db:
+            cur = db.cursor()
+            cur.execute(f"SELECT * FROM Password_Book WHERE Link LIKE '%{self.website}%'")
+            data = cur.fetchall()
+            if len(data) == 0:
+                sys.exit("Website not found.")
+            elif len(data)== 1:
+                for i in data:
+                    self.link = i[0]
+                    self.email = i[1]
+                    self.token = i[2]
+                    self.salt = i[3]
+            elif len(data) > 1:
+                k = 0
+                for i in data:
+                    print(f"[{k}] Link:{i[0]}, Email: {i[1]}")
+                    k = k+1
+                x = int(input("Select account: "))
+
+                self.link = data[x][0]
+                self.email = data[x][1]
+                self.token = data[x][2]
+                self.salt = data[x][3]
 
 
-sample_account = PasswordManager("abcde@gmail.com")
-sample_account2 = PasswordManager("abcde@outlook.com")
+        self.decrypt()
 
-sample_account.yourEmails()
-print("------------------------------")
+    def decrypt(self):
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=self.salt,
+            iterations=390000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(self.encryption_password))
+        f = Fernet(key)
+        self.password = f.decrypt(self.token).decode()
 
-sample_account.createPassword()
-print("email: " + sample_account.email)
-print("password: " + sample_account.password)
+example = SetAccount("abcd@gmail.com", "98742365")
 
+print(example.password)
 
-print("------------------------------")
+print("-----------")
 
-sample_account2.createPassword()
-print("email: " + sample_account2.email)
-print("password: " + sample_account2.password)
+x = GetAccount("98742365", "example")
+
+print(x.password)
